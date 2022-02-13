c0d1e0d49d76a68ca94b73de0ac87eac787aa124
fanlia <3093932086@qq.com>
2022-02-13 11:47:22 +0800

    refine test

--- test.js
+++ test.js
@@ -1,91 +1,13 @@
 
+const fs = require('fs')
 const parse = require('.')
 
-const source = `
-grammar
-    rules
+const source = fs.readFileSync('mckeeman.grammar', 'utf8')
 
-space
-    '0020'
-
-newline
-    '000A'
-
-name
-    letter
-    letter name
-
-letter
-    'a' . 'z'
-    'A' . 'Z'
-    '_'
-
-indentation
-    space space space space
-
-rules
-    rule
-    rule newline rules
-
-rule
-    name newline nothing alternatives
-
-nothing
-    ""
-    indentation '"' '"' newline
-
-alternatives
-    alternative
-    alternative alternatives
-
-alternative
-    indentation items newline
-
-items
-    item
-    item space items
-
-item
-    literal
-    name
-
-literal
-    singleton
-    range exclude
-    '"' characters '"'
-
-singleton
-    ''' codepoint '''
-
-codepoint
-    ' ' . '10FFFF'
-    hexcode
-
-hexcode
-    "10" hex hex hex hex
-    hex hex hex hex hex
-    hex hex hex hex
-
-hex
-    '0' . '9'
-    'A' . 'F'
-
-range
-    singleton space '.' space singleton
-
-exclude
-    ""
-    space '-' space singleton exclude
-    space '-' space range exclude
-
-characters
-    character
-    character characters
-
-character
-    ' ' . '10FFFF' - '"'
-
-`
 const result = parse(source)
+const resultStr = JSON.stringify(result, null, 2)
 
-console.log(JSON.stringify(result, null, 2))
+const expected = fs.readFileSync('mckeeman.json', 'utf8').trimRight()
+const passed = expected === resultStr
+console.log({passed})
+
