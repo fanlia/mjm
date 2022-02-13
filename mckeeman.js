a07fa6107be05db83a8a8018892f64deaa2406ed
fanlia <3093932086@qq.com>
2022-02-13 22:19:45 +0800

    refine mckeeman.js

--- mckeeman.js
+++ mckeeman.js
@@ -1,47 +1,190 @@
 
+const grammar = {
+    type: 'and',
+    predict: () => ([
+        whitespace,
+        rules,
+    ]),
+    fn: true,
+    format: (d) => d[1],
+}
+
 const space = {
     type: 'chars',
-    predict: [' '],
+    predict: () => ([' ']),
+    fn: true,
 }
 
 const newline = {
     type: 'chars',
-    predict: ['\n'],
+    predict: () => (['\n']),
+    fn: true,
+}
+
+const name = {
+    type: 'many',
+    predict: () => letter,
+    fn: true,
+    format: d => ({name: d.join('')}),
 }
 
 const letter = {
     type: 'char',
-    predict: ch => (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_',
+    predict: () => ch => (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_',
+    fn: true,
 }
 
 const indentation = {
     type: 'and',
-    predict: [
+    predict: () => ([
         space,
         space,
         space,
         space,
-    ],
+    ]),
+    fn: true,
 }
 
-const character = {
-    type: 'char',
-    predict: ch => (ch >= ' ' && ch.codePointAt(0) <= 0x10FFFF) && (ch !== '"'),
+const rules = {
+    type: 'until',
+    predict: () => ({
+        predict: rule,
+        hasNextPredict: newline,
+    }),
+    fn: true,
 }
 
-const characters = {
+const rule = {
+    type: 'and',
+    predict: () => ([
+        name,
+        newline,
+        nothing,
+        alternatives,
+    ]),
+    fn: true,
+    format: ([name, newline, nothing, alternatives]) => ({name: name.name, nothing: nothing.ok, alternatives}),
+}
+
+const nothing = {
+    type: 'maybe',
+    predict: () => ({
+        type: 'and',
+        predict: [
+            indentation,
+            {
+                type: 'chars',
+                predict: ['"'],
+            },
+            {
+                type: 'chars',
+                predict: ['"'],
+            },
+            newline,
+        ],
+    }),
+    fn: true,
+}
+
+const alternatives = {
     type: 'many',
-    predict: character,
+    predict: () => alternative,
+    fn: true,
 }
 
-const hex = {
-    type: 'char',
-    predict: ch => (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z'),
+const alternative = {
+    type: 'and',
+    predict: () => ([
+        indentation,
+        items,
+        newline,
+    ]),
+    fn: true,
+    format: ([indentation, items, newline]) => items,
+}
+
+const items = {
+    type: 'until',
+    predict: () => ({
+        predict: item,
+        hasNextPredict: space,
+    }),
+    fn: true,
+}
+
+const item = {
+    type: 'or',
+    predict: () => ([
+        literal,
+        name,
+    ]),
+    fn: true,
+}
+
+const literal = {
+    type: 'or',
+    predict: () => ([
+        {
+            type: 'and',
+            predict: [
+                range,
+                exclude,
+            ],
+            format: ([range, exclude]) => ({range, exclude: exclude.data})
+        },
+        singleton,
+        {
+            type: 'and',
+            predict: [
+                {
+                    type: 'chars',
+                    predict: ['"'],
+                },
+                characters,
+                {
+                    type: 'chars',
+                    predict: ['"'],
+                },
+            ],
+            format: d => ({characters: d[1].join('')}),
+        },
+    ]),
+    fn: true,
+}
+
+const singleton = {
+    type: 'and',
+    predict: () => ([
+        {
+            type: 'chars',
+            predict: ['\''],
+        },
+        codepoint,
+        {
+            type: 'chars',
+            predict: ['\''],
+        },
+    ]),
+    fn: true,
+    format: d => ({singleton: d[1]}),
+}
+
+const codepoint = {
+    type: 'or',
+    predict: () => ([
+        hexcode,
+        {
+            type: 'char',
+            predict: ch => ch >= ' ' && ch.codePointAt(0) <= 0x10FFFF,
+            format: ch => ({char: ch}),
+        },
+    ]),
+    fn: true,
 }
 
 const hexcode = {
     type: 'or',
-    predict: [
+    predict: () => ([
         {
             type: 'and',
             predict: [
@@ -74,41 +217,20 @@ const hexcode = {
                 hex,
             ],
         },
-    ],
+    ]),
+    fn: true,
     format: d => ({hexcode: [].concat(...d).join('')}),
 }
 
-const codepoint = {
-    type: 'or',
-    predict: [
-        hexcode,
-        {
-            type: 'char',
-            predict: ch => ch >= ' ' && ch.codePointAt(0) <= 0x10FFFF,
-            format: ch => ({char: ch}),
-        },
-    ],
-}
-
-const singleton = {
-    type: 'and',
-    predict: [
-        {
-            type: 'chars',
-            predict: ['\''],
-        },
-        codepoint,
-        {
-            type: 'chars',
-            predict: ['\''],
-        },
-    ],
-    format: d => ({singleton: d[1]}),
+const hex = {
+    type: 'char',
+    predict: () => ch => (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z'),
+    fn: true,
 }
 
 const range = {
     type: 'and',
-    predict: [
+    predict: () => ([
         singleton,
         space,
         {
@@ -117,13 +239,14 @@ const range = {
         },
         space,
         singleton,
-    ],
+    ]),
+    fn: true,
     format: ([start, s1, dot, s2, end]) => ({start: start.singleton, end: end.singleton})
 }
 
 const exclude = {
     type: 'maybe',
-    predict: {
+    predict: () => ({
         type: 'many',
         predict: {
             type: 'or',
@@ -155,112 +278,20 @@ const exclude = {
             ],
             format: d => d[3],
         },
-    },
+    }),
+    fn: true,
 }
 
-const literal = {
-    type: 'or',
-    predict: [
-        {
-            type: 'and',
-            predict: [
-                range,
-                exclude,
-            ],
-            format: ([range, exclude]) => ({range, exclude: exclude.data})
-        },
-        singleton,
-        {
-            type: 'and',
-            predict: [
-                {
-                    type: 'chars',
-                    predict: ['"'],
-                },
-                characters,
-                {
-                    type: 'chars',
-                    predict: ['"'],
-                },
-            ],
-            format: d => ({characters: d[1].join('')}),
-        },
-    ],
-}
-
-const name = {
+const characters = {
     type: 'many',
-    predict: letter,
-    format: d => ({name: d.join('')}),
+    predict: () => character,
+    fn: true,
 }
 
-const item = {
-    type: 'or',
-    predict: [
-        literal,
-        name,
-    ],
-}
-
-const items = {
-    type: 'until',
-    predict: {
-        predict: item,
-        hasNextPredict: space,
-    },
-}
-
-const alternative = {
-    type: 'and',
-    predict: [
-        indentation,
-        items,
-        newline,
-    ],
-    format: ([indentation, items, newline]) => items,
-}
-
-const alternatives = {
-    type: 'many',
-    predict: alternative,
-}
-
-const nothing = {
-    type: 'maybe',
-    predict: {
-        type: 'and',
-        predict: [
-            indentation,
-            {
-                type: 'chars',
-                predict: ['"'],
-            },
-            {
-                type: 'chars',
-                predict: ['"'],
-            },
-            newline,
-        ],
-    },
-}
-
-const rule = {
-    type: 'and',
-    predict: [
-        name,
-        newline,
-        nothing,
-        alternatives,
-    ],
-    format: ([name, newline, nothing, alternatives]) => ({name: name.name, nothing: nothing.ok, alternatives}),
-}
-
-const rules = {
-    type: 'until',
-    predict: {
-        predict: rule,
-        hasNextPredict: newline,
-    },
+const character = {
+    type: 'char',
+    predict: () => ch => (ch >= ' ' && ch.codePointAt(0) <= 0x10FFFF) && (ch !== '"'),
+    fn: true,
 }
 
 const whitespace = {
@@ -269,18 +300,9 @@ const whitespace = {
         type: 'many',
         predict: {
             type: 'char',
-            predict: ch => !letter.predict(ch),
+            predict: ch => /\s/.test(ch),
         },
     },
-}
-
-const grammar = {
-    type: 'and',
-    predict: [
-        whitespace,
-        rules,
-    ],
-    format: (d) => d[1],
 }
 
 module.exports = grammar
