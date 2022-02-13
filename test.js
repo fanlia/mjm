802a070d70c46bea33613503f2a074a57e9da008
fanlia <3093932086@qq.com>
2022-02-13 11:50:21 +0800

    refine test

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const source = fs.readFileSync('mckeeman.grammar', 'utf8')
 const result = parse(source)
 const resultStr = JSON.stringify(result, null, 2)
 
-const expected = fs.readFileSync('mckeeman.json', 'utf8').trimRight()
+const expected = fs.readFileSync('mckeeman.json', 'utf8')
 const passed = expected === resultStr
 console.log({passed})
 
