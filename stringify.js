5246e651629ae4ee741e9e96a7c8458c5bd44e0c
fanlia <3093932086@qq.com>
2022-02-15 22:06:52 +0800

    refact stringify

--- stringify.js
+++ stringify.js
@@ -31,80 +31,114 @@ const stringifyExclude = (exclude) => {
 
 const stringifyItem = (item) => {
     if (item.name) {
-        return item.name
+        return { type: 'name', data: item.name }
     } else if (item.characters) {
-        return  `{type: 'chars', predict: [${item.characters.split('').map(d => `'${d}'`).join(',')}]}`
+        const data = `{type: 'chars', predict: [${item.characters.split('').map(d => `'${d}'`).join(',')}]}`
+        return { type: 'characters', data }
     } else if (item.singleton) {
         const fnpart = stringifySingleton(item.singleton, '===')
-        return `{type: 'char', predict: ch => ${fnpart}}`
+        const data = `{type: 'char', predict: ch => ${fnpart}}`
+        return { type: 'singleton', data }
     } else if (item.range) {
         let fnpart = stringifyRange(item.range)
         if (item.exclude && item.exclude.length > 0) {
             fnpart += ` && !(${item.exclude.map(d => `(${stringifyExclude(d)})`).join(' || ')})`
         }
-        return `{type: 'char', predict: ch => ${fnpart}}`
+        const data = `{type: 'char', predict: ch => ${fnpart}}`
+        return { type: 'range', data }
     } else {
         throw new Error('not implemented item type')
     }
 }
 
-const stringifyAlternative = (items) => {
-    const results = items.map(stringifyItem)
+const convert = alternatives => alternatives.map(d => d.map(alternatives))
 
-    const space12 = ' '.repeat(12)
-    const space8 = ' '.repeat(8)
-    const space16 = ' '.repeat(16)
+const isEqual = (itemA, itemB) => itemA.type === itemB.type && itemA.data === itemB.data
 
-    if (results.length === 1) {
-        return results[0]
+const isEqualName = (item, name) => item.type === 'name' && item.data === name
+
+const isMany1 = (alternatives, name) => {
+    return alternatives.length === 2
+        && alternatives[0].length === 2
+        && alternatives[1].length === 1
+        && alternatives[0][0].type === 'name'
+        && isEqual(alternatives[0][0], alternatives[1][0])
+        && isEqualName(alternatives[0][1], name)
+}
+
+const isMany2 = (alternatives, name) => {
+    return alternatives.length >= 2 && alternatives.every(alternative => isEqualName(alternative.at(-1), name))
+}
+
+const isUntil = (alternatives, name) => {
+    return alternatives.length === 2
+        && alternatives[0].length === 3
+        && alternatives[1].length === 1
+        && alternatives[0][0].type === 'name'
+        && isEqual(alternatives[0][0], alternatives[1][0])
+        && isEqualName(alternatives[0][2], name)
+}
+
+const space4 = ' '.repeat(4)
+const space8 = ' '.repeat(8)
+const space12 = ' '.repeat(12)
+const space16 = ' '.repeat(16)
+
+const stringifyAlternatives = (alternatives) => {
+    const hasName = alternatives.some(items => items.some(item => item.type === 'name'))
+    if (alternatives.length === 1) {
+        const items = alternatives[0]
+        if (items.length === 1 && !hasName) {
+            return alternatives[0][0].data
+        }
+        const andPredict = items.map(item => item.data).join(`,\n${space8}`)
+        if (hasName) {
+            return `{\n${space4}type: "and",\n${space4}predict: () => ([\n${space8}${andPredict},\n${space4}]),\n${space4}fn: true,\n}`
+        } else {
+            return `{\n${space4}type: "and",\n${space4}predict: [\n${space8}${andPredict},\n${space4}]\n}`
+        }
     }
+    const orPredict = alternatives
+    .map(items => {
+        if (items.length === 1) {
+            return `${space8}${items[0].data}`
+        }
+        const andPredict = items.map(item => item.data).join(`,\n${space16}`)
+        return `${space8}{\n${space12}type: "and",\n${space12}predict: [\n${space16}${andPredict},\n${space12}]\n${space8}}`
+    })
+    .join(',\n')
 
-    return `{\n${space12}type: "and",\n${space12}predict: [\n${space16}${results.join(`,\n${space16}`)},\n${space12}],\n${space8}}`
+    if (hasName) {
+        return `{\n${space4}type: "or",\n${space4}predict: () => ([\n${orPredict},\n${space4}]),\n${space4}fn: true,\n}`
+    } else {
+        return `{\n${space4}type: "or",\n${space4}predict: [\n${orPredict},\n${space4}]\n}`
+    }
 }
 
 const stringifyRule = (rule) => {
 
     let { name, nothing, alternatives } = rule
 
-    alternatives = alternatives.sort((a, b) => b.length - a.length)
-    const noName = alternatives.every(alternative => alternative.every(item => item.name === undefined))
+    alternatives = alternatives.sort((a, b) => b.length - a.length).map(alternative => alternative.map(stringifyItem))
 
-    const space4 = ' '.repeat(4)
-    const space8 = ' '.repeat(8)
+    const noName = alternatives.every(alternative => alternative.every(item => item.type !== 'name'))
 
     let script = `const ${name} = `
 
     let body = null
 
-    let results = alternatives.map(stringifyAlternative)
-
-
-    if (noName) {
-        if (results.length === 1) {
-            body = results[0]
-        } else {
-            body = `{\n${space4}type: "or",\n${space4}predict: [\n${space4}${results.map(d => `${space4}${d}`).join(`,\n${space4}`)},\n${space4}],\n}`
-
-        }
+    if (isMany1(alternatives, name)) {
+        const [ item ] = alternatives[0]
+        body = `{type: "many", predict: () => ${item.data}, fn: true}`
+    } else if (isUntil(alternatives, name)) {
+        const [ item, nextItem ] = alternatives[0]
+        body = `{type: "until", predict: () => ({ predict: ${item.data}, hasNextPredict: ${nextItem.data} }), fn: true}`
+    } else if (isMany2(alternatives, name)) {
+        alternatives = alternatives.map(d => d.slice(0, -1))
+        const predict = stringifyAlternatives(alternatives)
+        body = `{type: "many", predict: ${predict}}`
     } else {
-
-        if (results.length === 1) {
-            const converted = results[0]
-                .replace(/ {16,16}/g, space8)
-                .replace(/ {12,12}/g, space4)
-                .replace(/ {8,8}\}/, '}')
-                .replace(/predict: \[/, 'predict: () => ([')
-                .replace(/],\n}$/, `]),\n${space4}fn: true,\n}`)
-
-            body = `${converted}`
-        } else {
-            body = `{\n${space4}type: "or",\n${space4}predict: () => ([\n${space4}${results.map(d => `${space4}${d}`).join(`,\n${space4}`)},\n${space4}]),\n${space4}fn: true,\n}`
-
-        }
-    }
-
-    if (!body) {
-        throw new Error('not implemented stringify logic')
+        body = stringifyAlternatives(alternatives)
     }
 
     if (nothing) {
